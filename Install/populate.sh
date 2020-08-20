32b5cead83ab5c2df3f3174b74879b10ac88308e
Chris K <chris.krofchak@gmail.com>
2020-08-20 18:31:04 -0400

    New populate script
    Simpler populate bash script (without sudo to download python).

--- Install/populate.sh
+++ Install/populate.sh
@@ -7,11 +7,6 @@
 # this will be more work since I will have to add depth by one 
 # and add an optional "1/0" field to see if they want us to make a venv
 
-PROJNAME=${1:-KleinProj}
-APPNAME=${2:-KleinApp}
-DIRNAME=${3:-KleinSrc}
-ENVNAME=${4:-venv}
-
 cyan=$(tput setaf 6)
 red=$(tput setaf 1)
 green=$(tput setaf 2)
@@ -57,7 +52,7 @@ echo ""
 
 # checks to make sure this has not already been run / a folder named the project name already exists
 
-if [ -d "$DIRNAME" ] ; then 
+if [ -d "Klein-Clientside" ] ; then 
     echo ""
     echo "${red}---"
     echo "Project directory already exists. Either change directory or directory name for new project folder. Exiting..."
@@ -65,9 +60,6 @@ if [ -d "$DIRNAME" ] ; then
     echo ""
     exit 1
 else 
-    mkdir "$DIRNAME"
-    cd "$DIRNAME"
-    echo ""
     echo "---"
     echo "${bold}Project directory made successfully. Now cloning Klein-Clientside.${reset}"
     echo "---"
@@ -76,138 +68,15 @@ fi
 
 # clones the source klein files 
 
-git clone -b kevin_testing 'https://github.com/mstachowsky/Klein-Clientside/'
+git clone -b master 'https://github.com/mstachowsky/Klein-Clientside/'
 
 echo ""
 echo "---"
-echo "${bold}Cloning complete. Making Django project.${reset}"
+echo "${bold}Cloning complete.${reset}"
 echo "---"
 echo ""
 
-# makes a project
-
-django-admin startproject "$PROJNAME"
-
-echo ""
-echo "---"
-echo "${bold}Project created, migrating.${reset}"
-echo "---"
-echo ""
-
-cd "$PROJNAME" 
-
-python manage.py makemigrations
-python manage.py migrate
-
-python manage.py startapp "$APPNAME"
-
-cd "$APPNAME"
-
-echo ""
-echo "---"
-echo "${bold}Migrations complete. Populating project with Klein templates.${reset}"
-echo "---"
-echo ""
-
-if [ -f views.py ]; then
-	rm views.py
-	echo "  Default/existing views.py removed successfully."
-else
-	echo "  No default views.py to remove."
-fi
-
-if [ -f urls.py ]; then
-	rm urls.py
-	echo "  Default/existing urls.py removed successfully."
-else
-	echo "  No default urls.py to remove."
-fi
-
-
-cd ../../Klein-Clientside/serverside/klein/
-
-DEST="../../../$PROJNAME/$APPNAME"
-
-# can add -v for 'verbose' description for debugging.
-
-cp -R ./static "$DEST"
-cp -R ./templates "$DEST"
-cp views.py "$DEST"
-cp urls.py "$DEST"
-
-cd ../../../$PROJNAME/$APPNAME
-
-if [ -f "urls.py" ] && [ -f "views.py" ] && [ -d "static" ] && [ -d "templates" ]; then
-    echo ""
-    echo "---"
-    echo "${bold}Files moved successfully.${reset}"
-    echo "---"
-    echo ""
-else 
-    echo ""
-    echo "${red}---"
-    echo "Files not moved. Exiting..."
-    echo "---${reset}"
-    echo ""
-    exit 1
-fi
-
-cd ../../$PROJNAME
-echo "$(pwd)"
-
-python manage.py makemigrations
-python manage.py migrate
-
-cd $PROJNAME
-echo "$(pwd)"
-
-echo "---"
-echo "${bold}Population complete. Appending django source files with necessary urls.${reset}"
-echo "---"
-echo ""
-
-CURRENTDIR="$(pwd)"
-
-python ../../../populate.py "$CURRENTDIR" $APPNAME
-
-ERR_ID=$?
-
-if [ $ERR_ID == 0 ]; then 
-    echo ""
-    echo "${bold}---"
-    echo "Appending lines successful. Making final migrations..."
-    echo "---${reset}"
-    echo ""
-elif [ $ERR_ID == 1 ]; then 
-    echo ""
-    echo "${red}---"
-    echo "Appending lines unsuccessful. You should never see this, this means populate.py was called with the wrong number of arguments."
-    echo "---${reset}"
-    echo ""
-    exit 1
-elif [ $ERR_ID == 2 ]; then 
-    echo ""
-    echo "${red}---"
-    echo "Appending lines unsuccessful. You should never see this, this means populate.py was called in the wrong folder, and urls.py and settings.py were not found."
-    echo "---${reset}"
-    echo ""
-    exit 1
-else 
-    echo ""
-    echo "${red}---"
-    echo "Something went wrong. All installs were correct except for adding lines to:"
-    echo "  urls.py"
-    echo "  - at the end of 'from django.urls import path', add ', include'."
-    echo "  - add 'path('AppName', include('AppName.urls'))' to 'urlpatterns' array."
-    echo ""
-    echo "  settings.py"
-    echo "  - add 'appname.apps.AppnameConfig' to INSTALLED_APPS array."
-    echo ""
-    echo "and you're all set."
-    echo "---${reset}"
-    echo ""
-    exit 1
-fi
+# done! 
 
 echo ""
 echo "${bold}${green}---"
