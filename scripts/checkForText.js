e0a5dd0fe2a63e29a79ddae1d18e7e7fef2ff131
glaukiol1 <glaukiol1@gmail.com>
2021-05-10 19:48:30 +0200

    Add license comments to scripts/checkForText.js

--- scripts/checkForText.js
+++ scripts/checkForText.js
@@ -13,3 +13,20 @@
     document.head.appendChild(script);
   }
 })();
+
+/*
+ * @license
+ * Licensed under the GNU GPLv3 License (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+
+ * Any libraries written by third parties are provided under a license that is identical to or compatible with the License on this project.
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License
+*/
